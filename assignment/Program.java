4f7413b58fc75fb8613b007c8f5d873dc849e5cc
AmaarM20 <114660087+AmaarM20@users.noreply.github.com>
2022-11-01 10:48:34 +0000

    started the assignment

---
+++ assignment/Program.java
@@ -0,0 +1,19 @@
+
+
+import helpers.InputReader;
+public class Program
+{
+    
+    public static void main(String[] args)
+    {
+
+        System.out.println("Amaar's CW1 ");
+
+        String name = InputReader.getString("enter your name >amaar");
+        System.out.println("Hello " + name);
+
+        
+        int id = InputReader.getInt("Enter your id >");
+        System.out.println("enter your id" + id);
+    } 
+}
