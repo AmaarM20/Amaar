063d910262639f8528cf0887ba157c7dd12a48d4
AmaarM20 <114660087+AmaarM20@users.noreply.github.com>
2022-11-06 13:17:39 +0000

    updated assignment

--- assignment/Program.java
+++ assignment/Program.java
@@ -1,5 +1,7 @@
 
 
+import java.util.ArrayList;
+
 import helpers.InputReader;
 public class Program
 {
@@ -16,11 +18,110 @@ public class Program
         //int id = InputReader.getInt("Enter your id >");
         //System.out.println("enter your id" + id);
 
-        String title = InputReader.getString("song title >");
-        System.out.println("Title: " + title);
+        //String atrist = InputReader.getString(" artist > ");
+        //System.out.println("artist: " + 1);
 
-        String artist = InputReader .getString("artist >");
-        System.out.println("Artist:" + artist);
+        //String name = InputReader .getString("song name >");
+       // System.out.println("song:" + name );
+
+       // String views = InputReader.getString("Enter your views >");
+       // System.out.println("views:" + views);
+
+       // ArrayList<Integer> numbers = new ArrayList<Integer>();
+
+       // numbers.add(1);
+       // numbers.add(2);
+       // numbers.add(3);
+
+        
+     //System.out.println(numbers);
+
+     ArrayList artists = new ArrayList();
+
+     artists.add("Artist 1> 50 Cent");
+     artists.add(" Artist 2> Suspect");
+     artists.add("Artist 3> YB");
+     artists.remove("Artist 3> YB");
+     artists.add("Artist 4> Taylor swift");
+     artists.add("Artist 5> Nicki Minaj");
+     artists.add("Artist 6> Nitonb");
+     artists.add("Artist 7> Sayso");
+     artists.add("Artist 8> Takeoff RIP");
+     artists.add("Artist 9> Nipsey hussle");
+     artists.add("Artist 10> CB");
+
+     System.out.println(artists);
+
+     ArrayList songs = new ArrayList();
+
+     songs.add("Artist 1> In da Club");
+     songs.add("Artist 2> No Hook");
+     songs.add("Artist 3> DONT MAKE SENSE");
+     songs.remove("Artist 3> DONT MAKE SENSE");
+     songs.add("Artist 4> Anti Hero");
+     songs.add("Artist 5> Moment 4 life");
+     songs.add("Artist 6> Rise & Tan");
+     songs.add("Artist 7> Spinner");
+     songs.add("Artist 8> Last memory");
+     songs.add("Artist 9> Victory Lap");
+     songs.add("Artist 10> Take that risk");
+
+     System.out.println(songs);
+
+     ArrayList views = new ArrayList();
+
+     views.add("Artist 1> 10000000");
+     views.add("Artist 2> 945633");
+     views.add("Artist 3> 345658");
+     views.remove("Artist 3> 345658");
+     views.add("Artist 4> 200000000");
+     views.add("Artist 5> 456793");
+     views.add("Artist 6> 32423487");
+     views.add("Artist 7> 53874");
+     views.add("Artist 8> 345785");
+     views.add("Artist 9> 9999999");
+     views.add("Artist 10> 59843");
+
+     System.out.println(views);
+
+
+
+
+
+
+
+
+
+
+
+            
+        
+
+    
+
+
+
+
+
+    
+        
+        
+
+
+
+            
+        
+
+
+
+
+
+
+
+
+
+
+
 
     } 
 
